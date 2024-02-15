7e7d76295c4f1139f47ee82d028c402791679827
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2024-02-16 11:05:23 +1300

    done task 2

--- MGS Childcare/auction.v1.py
+++ MGS Childcare/auction.v1.py
@@ -5,19 +5,16 @@ reserve_price = float(input("What is the reserve price?:"))
 print(f"The auction for {auction_item} has started!\n")
 while True:
     print(f"The Highest bid right now is ${bid}")
-    bidder = float(input("What is your bid (-1 to exit auction): "
-                         "$")).__round__(2)
-    if bid >= bidder:
-        print("Please bid a higher number\n")
+    bidder = round(float(input("What is your bid (-1 to exit auction): "
+                         "$")), 2)
+    if bidder < 0:
+        break
     elif bid < bidder:
         bid = bidder
-    elif bidder == -1.0:
-        break
+    elif bid >= bidder:
+        print("Please bid a higher number\n")
 if bid > reserve_price:
-    print(f"The auction for {auction_item} has finished with a bid of ${bid}")
+    print(f"The auction for {auction_item} has finished with a bid of $"
+          f"{round(bid, 2)}")
 else:
-    print("project")
-
-
-
-
+    print(f"The {auction_item} did not sell")
